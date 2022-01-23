380ffd47c7a1f2aff1ef44ee6b5e7121ef051ee0
Matthias Blum <m.blum@student.uni-tuebingen.de>
2022-01-23 21:01:09 +0100

    Testing efficient line mask algo

--- test.py
+++ test.py
@@ -1,19 +1,22 @@
 import matplotlib.pyplot as plt
 import torch
 import numpy as np
+import time
 
 from model import PartialConvNet
-from prep_data_lines_testing import PrepData
+from prep_data_efficient_lines_testing import PrepData
+
+start = time.time()
 
 device = torch.device('cpu')
 
 model = PartialConvNet().double()
-model.load_state_dict(torch.load('model_first_cycle.t7', map_location=device))
+model.load_state_dict(torch.load('model_epoch_13_rectangles.t7', map_location=device))
 model = model.to(device)
 model.eval()
 
 #rand = np.random.randint(0, 159)
-img, mask, gt_img = PrepData()[1]
+img, mask, gt_img = PrepData()[0]
 
 #plt.imshow(img.permute(1, 2, 0))
 # See ground truth images
@@ -36,5 +39,8 @@ output = np.interp(output, (output.min(), output.max()), (0., 1.))
 #print(output)
 
 #plt.imshow(output[0].permute(1, 2, 0))
-plt.imsave('test_batchsize32_rectangle.png', output)
+plt.imsave('test_efficiency_new.png', output)
 #plt.show()
+end = time.time()
+
+print("The time of execution of the test is: ", end-start)
